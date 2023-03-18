9339360c7af3f961c88213e76b7c454670b64866
Elaina <elainabubble@gmail.com>
2023-03-18 01:12:05 +0100

    checking last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+*main - Entry point
+*Return: always 0 (success)
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+int n;
+int lastDigit = n % 10;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (lastDigit < 6 && n != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+else if (lastDigit > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+else
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,5 @@
 0-Script that displays a random number and prints out whether it's positive, negative or equals 0
 
+1-// that takes the last digit of the variable n and checks if it's zero, less than 6 and not 0, or greater than 5
+
 
