6f3e490ce382556f982014d8d0b5465d8124a68c
Elaina <elainabubble@gmail.com>
2023-03-18 03:07:22 +0100

    alphabet with exceptions

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,12 +7,14 @@
 int main(void)
 {
 char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++
+{)
 if (letter == 'q' || letter == 'e')
 {
 continue;
 }
 putchar(letter);
+}
 putchar('\n');
 return (0);
 }
