d14147a4ce7a1a03e08e3b81a5c8401fff676f21
Elaina <elainabubble@gmail.com>
2023-03-21 21:21:09 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: always 0 (success)
+*/
+int main(void)
+{
+char text[] = "_putchar";
+_putchar(text[0]);
+_putchar(text[1]);
+_putchar(text[2]);
+_putchar(text[3]);
+_putchar(text[4]);
+_putchar(text[5]);
+_putchar(text[6]);
+_putchar('\n');
+return (0);
+}
