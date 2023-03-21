05e726ede942ac84e608443486066791f60f6eea
Elaina <elainabubble@gmail.com>
2023-03-21 22:42:55 +0000

    alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,10 @@ void print_alphabet_x10(void)
 int i, e;
 for (i = 0; i <= 9; i++)
 {
-	for(e = 97; e <= 122; e++)
-	{
-		_putchar(e);
-	}
+for(e = 97; e <= 122; e++)
+{
+_putchar(e);
 }
 _putchar('\n');
+}
 }
