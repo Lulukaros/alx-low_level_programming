ebf619c782ab2c46362397d807aa08d3e6fa844b
Elaina <elainabubble@gmail.com>
2023-03-22 02:06:32 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*print_last_digit - returns the last digit
+*@a: the number that needs to be checked
+*Return: the last digit of a number
+*/
+int print_last_digit(int a)
+{
+return (a % 10);
+}
