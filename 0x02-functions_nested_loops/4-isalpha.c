da8e728867e2482110a95b628dbb1e223a451d2d
Elaina <elainabubble@gmail.com>
2023-03-22 00:55:38 +0000

    alpha or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isalpha - checks if a character is an alphabetic character
+*@c: the character to be checked
+*Return: always 1 if it's a letter, 0 if otherwise
+*/
+int _isalpha(int c)
+{
+if (c <= 'a' && c >= 'z')
+|| (c <= 'A' && c >= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
