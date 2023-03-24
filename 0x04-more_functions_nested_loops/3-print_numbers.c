f5ac809f7f17a322fd6548134989239e62aab7d0
Elaina <elainabubble@gmail.com>
2023-03-24 01:49:13 +0000

    numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_numbers - prints out the numbers from 0 to 9
+*Return: 0 (success)
+*/
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; i++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+
+	return (0);
+}
