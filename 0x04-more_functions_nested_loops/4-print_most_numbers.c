e88e4d517e5b9cf9267e0d73f7e29247898fed03
Elaina <elainabubble@gmail.com>
2023-03-24 02:54:32 +0000

    0-9 with exceptions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*print_numbers - prints out the numbers from 0 to 9 except 2*and 4
+*print_most_numbers - prints out the numbers from 0 to 9 except 2*and 4
 *Return: void
 */
 void print_most_numbers(void)
