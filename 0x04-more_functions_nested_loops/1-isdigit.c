47aadb7c3590b9aa4bb105fc9c10e798b9a592b3
Elaina <elainabubble@gmail.com>
2023-03-24 01:06:20 +0000

    digit checker

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_isdigit - Checks if c is a digit
+*@c: the character to be checked
+*Return: (10) (success)
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (10)
+}
