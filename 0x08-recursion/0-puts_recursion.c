d3d7fd57e5176f2f5163cf20ebe3f19a546c08a8
Elaina <elainabubble@gmail.com>
2023-04-07 16:57:06 +0000

    My first Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,19 @@
 #include "main.h"
+
 /**
- *_puts_recursion - Prints a character
- *@s: pointer to the string
- *Return: string
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		{
-	_putchar('\n');
-	return;
-		}
-	putchar(*s);
+	if (*s == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
