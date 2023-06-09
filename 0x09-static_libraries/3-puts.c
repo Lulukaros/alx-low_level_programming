0a75f96c92c70d6fc397f516d1bab1d86fa14d78
Elaina <elainabubble@gmail.com>
2023-06-10 00:34:20 +0100

    Creating a library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string to stdout
+ * @str: input string
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
