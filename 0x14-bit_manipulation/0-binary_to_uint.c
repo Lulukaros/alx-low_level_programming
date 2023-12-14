72bf02d486fc037a7ccf64cf7d544bf2a886e04f
Elaina <elainabubble@gmail.com>
2023-12-14 18:45:00 +0100

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - oui oui si si
+ * @b: array containing the binary
+ * Return: decimal value
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int decival = 0;
+int i;
+
+if (!b)
+return (0);
+for (i = 0; i <= b[i]; i++)
+{
+	if (b[i] < 0 || b[i] > 1)
+		return (1);
+	decival = decival * 2 + (b[i] - '0');
+}
+return (decival);
+}
