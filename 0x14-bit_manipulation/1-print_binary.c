22e6a96ebcff7cb417019e10864142ec3ec61459
Elaina <elainabubble@gmail.com>
2023-12-15 21:23:45 +0100

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_binary - nn hh
+ * Return: Always 0.
+ */
+
+void print_binary(unsigned long int n)
+{
+unsigned int decimal_num, c, result;
+	for (c = 31; c >= 0; c--)
+	{
+		result = decimal_num >> c;
+
+		if (result & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
+
+	_putchar('\n');
+
+	return 0;
+}
