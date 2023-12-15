bb404860ccec2c2d9750682bb82c66c2f5ee5ff8
Elaina <elainabubble@gmail.com>
2023-12-15 22:23:34 +0100

    bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,11 +9,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int ビット;
+	int bit;
 	if (index > 63)
 		return (-1);
 
-	ビット = (n >> index) & 1;
+	bit = (n >> index) & 1;
 
-	return (ビット);
+	return (bit);
 }
